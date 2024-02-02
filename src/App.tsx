0693f173cce54e742669996047df1fd627bb86a1
Nischit D S <nischit819@gmail.com>
2024-02-02 10:31:17 +0530

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ function App() {
     <Grid templateAreas={{
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
-    }}>
+    }}
+    templateColumns={{
+      base: '1fr',
+      lg: '200px 1fr'
+    }}
+    >
       <GridItem area='nav'>
       <NavBar/>
       </GridItem>
-      <Show above="lg"><GridItem area='aside'><GenreList/></GridItem></Show> 
+      <Show above="lg"><GridItem area='aside' paddingX={5}><GenreList/></GridItem></Show> 
       <GridItem area='main'><GameGrid/></GridItem>
     </Grid>
   )
