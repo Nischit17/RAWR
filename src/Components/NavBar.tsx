fd094191d5a3c5db3b762006b783d8d84aeeee22
Nischit D S <nischit819@gmail.com>
2024-02-05 11:08:26 +0530

    Build Search Input

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,14 +1,16 @@
-import { HStack, Image } from '@chakra-ui/react'
-import Game_icon from  ".././assets/game-controller.png"
-import ColorModeSwitch from './ColorModeSwitch'
+import { HStack, Image } from "@chakra-ui/react";
+import Game_icon from ".././assets/game-controller.png";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-        <Image boxSize="60px" alt='navbar_img' src={Game_icon} />
-        <ColorModeSwitch/>
+    <HStack padding="10px">
+      <Image boxSize="60px" alt="navbar_img" src={Game_icon} />
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
